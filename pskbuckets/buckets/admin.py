f90a6a8deb86c3be0a6d414c7151ef3da45eccea
Patricio Soto Kort <psotokort@gmail.com>
2021-02-07 14:03:48 +0800

    Register models in admin

--- pskbuckets/buckets/admin.py
+++ pskbuckets/buckets/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from safedelete.admin import SafeDeleteAdmin, highlight_deleted
+from .models import Bucket, BankAccount, Category, Transaction
 
-# Register your models here.
+
+@admin.register(Bucket)
+class BucketAdmin(SafeDeleteAdmin):
+    list_display = (highlight_deleted, "name") + SafeDeleteAdmin.list_display
+    list_filter = SafeDeleteAdmin.list_filter
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(SafeDeleteAdmin):
+    list_display = (highlight_deleted, "name") + SafeDeleteAdmin.list_display
+    list_filter = SafeDeleteAdmin.list_filter
+
+
+@admin.register(Category)
+class CategoryAdmin(SafeDeleteAdmin):
+    list_display = (highlight_deleted, "name") + SafeDeleteAdmin.list_display
+    list_filter = SafeDeleteAdmin.list_filter
+
+
+@admin.register(Transaction)
+class TransactionAdmin(SafeDeleteAdmin):
+    list_display = (
+        highlight_deleted,
+        "name",
+        "get_bucket",
+    ) + SafeDeleteAdmin.list_display
+    list_filter = (
+        "bucket__name",
+        "bucket__bank_account__name",
+        "category__name",
+    ) + SafeDeleteAdmin.list_filter
+
+    def get_bucket(self, obj):
+        return obj.bucket.name
